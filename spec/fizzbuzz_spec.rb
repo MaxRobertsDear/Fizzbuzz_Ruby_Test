8e662f573175077a688149506bb52244ac54c8b9
MaxRobertsDear <maxhroberts@gmail.com>
2019-03-27 18:59:59 +0000

    Completed fizzbuzz TDD challenge

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,5 +12,13 @@ describe "fizzbuzz" do
     expect(fizzbuzz(5)).to eq 'buzz'
     expect(fizzbuzz(10)).to eq 'buzz'
   end 
+
+  it "returns 'fizzbuzz' when passed multiples of both 3 and 5" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  
+  it "return '7' when passed 7" do 
+    expect(fizzbuzz(7)).to eq 7
+  end
 end
 
